c45c01e074519437a4866ed55365826d7841ce6e
jbme <237287447+jbmey@users.noreply.github.com>
2025-11-18 14:26:15 +0100

    Refactor SudokuController to use a single instance of SudokuSolver for solving puzzles

--- src/main/java/com/jbme/sudoku_solver/SudokuController.java
+++ src/main/java/com/jbme/sudoku_solver/SudokuController.java
@@ -1,5 +1,6 @@
 package com.jbme.sudoku_solver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,10 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("/api/sudoku")
 public class SudokuController {
     private static final Logger log = LoggerFactory.getLogger(SudokuController.class);
+
+    @Autowired
+    private SudokuSolver solver;
+
      /**
      * REST controller to Solve a Sudoku grid
      * @param grid 9x9 array of integers (0 for empty cells)
@@ -19,14 +24,12 @@ public class SudokuController {
         log.info("Received Sudoku grid for solving");
         
         // First solution (ascending)
-        SudokuSolver solver1 = new SudokuSolver();
-        solver1.setGrid(copyGrid(grid));
-        int[][] result1 = solver1.fillSudokuGrid(1);
+        solver.setGrid(copyGrid(grid));
+        int[][] result1 = solver.fillSudokuGrid(1);
         
         // Second solution (descending)
-        SudokuSolver solver2 = new SudokuSolver();
-        solver2.setGrid(copyGrid(grid));
-        int[][] result2 = solver2.fillSudokuGrid(2);
+        solver.setGrid(copyGrid(grid));
+        int[][] result2 = solver.fillSudokuGrid(2);
         
         return new SudokuResponse(result1, result2);
     }

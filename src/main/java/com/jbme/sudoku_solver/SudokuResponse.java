bfbaa77ec6d0acbcd69bf3a85a447f539fc5d73b
jbme <237287447+jbmey@users.noreply.github.com>
2025-11-18 14:26:27 +0100

    Refactor SudokuResponse to initialize noSolution and improve checkNoSolution logic

--- src/main/java/com/jbme/sudoku_solver/SudokuResponse.java
+++ src/main/java/com/jbme/sudoku_solver/SudokuResponse.java
@@ -4,11 +4,13 @@ public class SudokuResponse {
      private int[][] solution1;
     private int[][] solution2;
     private boolean multiplesSolutions;
+    private boolean noSolution;
     
     public SudokuResponse(int[][] solution1, int[][] solution2) {
         this.solution1 = solution1;
         this.solution2 = solution2;
         this.multiplesSolutions = !gridEquals(solution1, solution2);
+        this.noSolution = checkNoSolution(solution1, solution2);
     }
     
     private boolean gridEquals(int[][] grid1, int[][] grid2) {
@@ -20,11 +22,20 @@ public class SudokuResponse {
         }
         return true;
     }
+
+    private boolean checkNoSolution(int[][] grid1, int[][] grid2){
+        if(grid1 == null && grid2 == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
     // Getters
     public int[][] getSolution1() { return solution1; }
     public int[][] getSolution2() { return solution2; }
     public boolean isMultiplesSolutions() { return multiplesSolutions; }
+    public boolean isNoSolution() {return noSolution;}
 }
 
 
